4c3f738be94b070e2dff2af81a10bb4500dc9466
timos <timoshkovalex@gmail.com>
2025-12-11 21:31:35 +0300

    third one

--- pow2 1000.fsx
+++ pow2 1000.fsx
@@ -11,10 +11,10 @@ module TailRecursive =
         else
             let digit = int (n % bigint 10)
             sumDigitsAcc (n / bigint 10) (acc + digit)
-    
+
     let sumDigits n = sumDigitsAcc n 0
-    
-    let solve() =
+
+    let solve () =
         let power = bigint.Pow(bigint 2, 1000)
         sumDigits power
 
@@ -26,8 +26,8 @@ module NonTailRecursive =
         else
             let digit = int (n % bigint 10)
             digit + sumDigits (n / bigint 10)
-    
-    let solve() =
+
+    let solve () =
         let power = bigint.Pow(bigint 2, 1000)
         sumDigits power
 
@@ -40,42 +40,39 @@ module Modular =
                     yield int (num % bigint 10)
                     yield! generate (num / bigint 10)
             }
+
         generate n
-    
-    let foldDigits digits =
-        digits |> Seq.fold (+) 0
-    
-    let solve() =
+
+    let foldDigits digits = digits |> Seq.fold (+) 0
+
+    let solve () =
         let power = bigint.Pow(bigint 2, 1000)
-        power
-        |> generateDigits
-        |> foldDigits
+        power |> generateDigits |> foldDigits
 
 // Маппинг
 module MapBased =
-    let solve() =
+    let solve () =
         let power = bigint.Pow(bigint 2, 1000)
         let powerStr = power.ToString()
-        powerStr
-        |> Seq.map (fun c -> int c - int '0')
-        |> Seq.sum
+        powerStr |> Seq.map (fun c -> int c - int '0') |> Seq.sum
 // for в функциональном стиле
 module LoopSyntax =
-    
-    let solve() =
+
+    let solve () =
         let power = bigint.Pow(bigint 2, 1000)
         let powerStr = power.ToString()
-        
+
         let mutable sum = 0
+
         for c in powerStr do
             sum <- sum + (int c - int '0')
+
         sum
 
 // ленивые вычисления и бесконечные списки
 module LazyInfinite =
-    let powersOfTwo = 
-        Seq.initInfinite (fun i -> bigint.Pow(bigint 2, i))
-    
+    let powersOfTwo = Seq.initInfinite (fun i -> bigint.Pow(bigint 2, i))
+
     let digitsLazy (n: bigint) =
         n
         |> Seq.unfold (fun state ->
@@ -83,8 +80,7 @@ module LazyInfinite =
                 None
             else
                 Some(int (state % bigint 10), state / bigint 10))
-    
-    let solve() =
+
+    let solve () =
         let power = powersOfTwo |> Seq.item 1000
         power |> digitsLazy |> Seq.sum
-
